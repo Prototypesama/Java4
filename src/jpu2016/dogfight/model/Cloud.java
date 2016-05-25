65af24cf11f00864ed4f9168140cf1b81f55efdc
Bluedin <pblueway@gmail.com>
2016-05-25 16:34:16 +0200

    Images OK

--- src/jpu2016/dogfight/model/Cloud.java
+++ src/jpu2016/dogfight/model/Cloud.java
@@ -1,10 +1,6 @@
 package jpu2016.dogfight.model;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 public class Cloud extends Mobile {
 
@@ -12,19 +8,9 @@ public class Cloud extends Mobile {
 	static int WIDTH = 300;
 	static int HEIGHT = 150;
 	static String IMAGE = "cloud.png";
-	private BufferedImage image;
 
 	public Cloud(Direction direction, Position position) throws IOException {
 		super(direction, position, new Dimension(WIDTH, HEIGHT), SPEED, IMAGE);
-		try {
-
-			this.image = ImageIO.read(new File("images/" + IMAGE));
-
-		} catch (final IOException e) {
-
-			e.printStackTrace();
-
-		}
 	}
 
 }
